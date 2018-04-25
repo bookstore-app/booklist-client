537907c3645c22534ba4bd258f4bbe7222a3834b
Max <gusstorm132@gmail.com>
2018-04-25 13:38:26 -0700

    filled out book-view.js

--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -6,18 +6,45 @@ var app = app || {};
   const booksView = {};
 
   function showNav(section) {
+    console.log('index');
     $('section').not(`#${section}`).hide();
     $(`#${section}`).show();
   }
 
   booksView.initIndexPage = function (ctx) {
+    console.log('index');
     $('items url').empty();
     showNav('items');
-    app.Task.all.forEach(task =>
-      $('#items ul').apprend(task.toHtml())
+    app.Book.all.forEach(book =>
+      $('#items ul').apprend(book.toHtml())
     );
+  };
+
+  booksView.initAddPage = function (ctx) {
+    console.log('add');
+    showNav('add');
+  };
+
+  booksView.initBookPage = function (ctx) {
+    console.log('view book', ctx.params.id);
+    $('#book').empty();
+    showNav('task');
+    app.Book.all.forEach(book => {
+      if (parseInt(book.id) === parseInt(ctx.params.id)) {
+        $('#book').append(book.detailToHtml());
+      }
+    });
+  };
+
+  $('#add form').on('submit', createNewBook);
+  function createNewBook(event) {
+    event.preventDefault();
+    let book = { book: event.target.book.value };
+    $.post(`${ENV.apiUrl}/api/v1/books`, book)
+      .then(app.Book.fetchAll(booksView.initIndexPage)
+      )
+      .catch(console.error);
   }
 
-  booksView.initAddPage
-
+  module.booksView = booksView;
 })(app);
