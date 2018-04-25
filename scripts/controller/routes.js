ae9fb3f876a756d66a41a0efb5d48c5db11dd12d
Collin Hintzke <collinhintzke5@gmail.com>
2018-04-25 14:27:41 -0700

    added controller with route.js

---
+++ scripts/controller/routes.js
@@ -0,0 +1,5 @@
+page( '/book-list-client/', () => app.Book.fetchAll(app.booksView.initIndexPage) );
+page( '/book-list-client/book/:id', ctx => app.booksView.initBookPage(ctx) );
+page( '/book-list-client/add', ctx => app.booksView.initAddPage(ctx) );
+
+page();
